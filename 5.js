195611f8c74202306601314285d47f232514188d
Tiago Lima <tiago.jlima@yahoo.com.br>
2019-12-21 21:31:05 -0300

    usando pipe

--- 5.js
+++ 5.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const gzip = require('zlib').createGzip(); //duplex
 
 // padrao é 65k
-const strmReadFile = fs.createReadStream('./combos_kof_2k2.mp4', { highWaterMark: 1*1024*1024 });
+const strmReadFile = fs.createReadStream('./combos_kof_2k2.mp4');
 const strmWriteFile = fs.createWriteStream('./combos_kof_2k2_2.mp4.zip');
 
 strmReadFile.pipe(gzip).pipe(strmWriteFile);
